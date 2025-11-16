2fa5139ad96000f22ba0db046bbab3fdfbcef28d
Joseph Moore <moorejose.work@gmail.com>
2025-11-16 16:43:07 +1100

    Import original Project 1 (animal.py, README.md) from 2022

---
+++ projects/project-1/animal.py
@@ -0,0 +1,8 @@
+# Author: Joseph Moore
+# GitHub username: JM7192
+# Date: 01/04/22
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
